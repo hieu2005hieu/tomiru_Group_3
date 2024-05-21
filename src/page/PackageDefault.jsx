f475cafeb1c7b7e7f0b5d1638932a9f99354ef19
unknown <tgdd.hieu2005@gmail.com>
2024-05-21 14:47:19 +0700

    addproject

---
+++ src/page/PackageDefault.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function PackageDefault() {
+  return (
+    <>
+      <div>
+        <div>
+          <h2 className='text-center'>gói - Default</h2>
+          <p>Thời hạn sử dụng 03 : 12 : 24 : 60</p>
+          <button>Nâng cấp lên Gói Thành viên</button>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </>
+  );
+}
